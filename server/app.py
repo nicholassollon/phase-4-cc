c4a5c4e5536363116a2012cbac5c53bf09f22fde
Nick Sollon <genericjoke@gmail.com>
2023-03-23 11:58:31 -0400

    Almost all

--- server/app.py
+++ server/app.py
@@ -17,14 +17,66 @@ db.init_app(app)
 
 api = Api(app)
 
+
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
-@app.route('/restaurants')
-def restaurants():
 
-    pass
+@app.route('/restaurants', methods=['GET'])
+def restaurants():
+    restaurants = Restaurant.query.all()
+    rest_dict = [restaurant.to_dict(
+        rules=('-restaurant_pizzas',))for restaurant in restaurants]
+    response = make_response(jsonify(rest_dict), 200)
+    return response
+
+
+@app.route('/restaurants/<int:id>', methods=['GET', 'DELETE'])
+def restaurantsById(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+    if restaurant:
+        if request.method == 'GET':
+                rest_dict = restaurant.to_dict(rules=('-restaurant_pizzas',))
+                response = make_response(jsonify(rest_dict), 200)
+        elif request.method == 'DELETE':
+             
+             response = make_response(jsonify(rest_dict), 200)
+    else:
+            response = make_response(jsonify("Restaurant not found"), 404)
+    return response
+
+
+@app.route('/pizzas', methods=['GET',])
+def pizzas():
+    pizzas = Pizza.query.all()
+    pizza_dict = [pizza.to_dict(
+        rules=('-restaurant_pizzas',))for pizza in pizzas]
+    response = make_response(jsonify(pizza_dict), 200)
+    return response
+
+@app.route('/restaurant_pizzas', methods=['POST',])
+def restaurantPizzaPost():
+    try:
+        new_rest_pizza = RestaurantPizza(price = request.get_json()['price'],
+                                         pizza_id = request.get_json()['pizza_id'],
+                                         restaurant_id = request.get_json()['restaurant_id'])
+        
+        db.session.add(new_rest_pizza) 
+        db.session.commit()
+
+        #not sure why but I get null back from this in postman 
+        pizza = Pizza.query.filter_by(id=new_rest_pizza.id).first()
+        response = make_response(jsonify(pizza),201)
+        #I also tried this but that didnt work. The tests won't help me either as they continuously give me nonetype error.
+        #pizza = Pizza.query.filter_by(id=new_rest_pizza.id).first()
+        #pizza = pizza.to_dict(rules=('-restaurant_pizzas',))
+        #response = make_response(jsonify(pizza),201)
+
+    except ValueError:
+         response = make_response(jsonify({"errors":"validation errors"}),400)
+    
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -10,4 +10,45 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# Add models here
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'pizza')
+    serialize_rules = ('-restaurant_pizza.pizzas','-restaurant.pizzas')
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref = 'restaurant')
+    serialize_rules = ('-pizza.restaurants','-restaurant_pizza.restaurants')
+
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    serialize_rules = ('-pizza.restaurant_pizzas','-restaurant.restaurant_pizzas')
+
+    @validates('price')
+    def validates_price(self,key,price):
+        if not price or price < 1 or price > 30:
+            raise ValueError("Price must be between 1 and 30")
+        else:
+            return price
